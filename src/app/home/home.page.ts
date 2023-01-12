3f369b3f81989db1274c599aa0e48387591d5941
Serhii Vidernikov <sv54@mscloud.ua.es>
2023-01-12 15:23:54 +0100

    Cambios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -27,27 +27,27 @@ export class HomePage {
         this.sendLocalNotification()
     }
 
-    async toggle(permiso: any){
-      this.enviarNotif = permiso.checked
-      console.log(this.enviarNotif)
-    }
+
 
     async setToday() {
         this.formattedDateTime = format(parseISO(format(Date.now(), 'yyyy-MM-dd HH:mm')), 'HH:mm,  dd/MM/yy')
         this.day = format(parseISO(format(Date.now(), 'yyyy-MM-dd HH:mm')), 'dd/MM/yy')
         this.vecesHoy = parseInt(await this.StorageService.getData(this.day))
         console.log(this.vecesHoy)
+        this.asignarTexto()
     }
 
     async addOne() {
         console.log(this.enviarNotif)
         this.vecesHoy = this.vecesHoy + 1
         await this.StorageService.addData(this.day, this.vecesHoy)
+        this.asignarTexto()
     }
     async minusOne(){
       if(this.vecesHoy > 0){
         this.vecesHoy = this.vecesHoy - 1
         await this.StorageService.addData(this.day, this.vecesHoy)
+        this.asignarTexto()
       }
     }
 
@@ -67,11 +67,34 @@ export class HomePage {
         await this.localNotification.DefaultNotification()
     }
 
+    async toggle(permiso: any){
+      this.enviarNotif = permiso.checked
+      console.log(this.enviarNotif)
+    }
+
     async sendLocalNotificationNow() {
-        console.log("Enviando notificacion")
+      console.log("Enviando notificacion")
       const randomId = Math.floor(Math.random() * 10000) + 1;
       this.body = "It's time to drink some water! 8 glasses of water is the goal!"
       await this.localNotification.showLocalNotification(randomId, "Time for a glass of water", this.body, this.h, Date.now() + 1000)
+    }
+
+    asignarTexto(){
+      if(this.vecesHoy == 0){
+        this.texto = "Es hora de comenzar el dia! "
+      }
+      else if(this.vecesHoy < 3 && this.vecesHoy > 0){
+        this.texto = "Bueno comienzo! Llevas: "
+      }
+      else if(this.vecesHoy > 2 && this.vecesHoy < 6){
+        this.texto = "LLevas buen ritmo! Sigue asi! Ya son: "
+      }
+      else if(this.vecesHoy > 5 && this.vecesHoy < 8){
+        this.texto = "Ya casi esta por hoy! Ya van: "
+      }
+      else if(this.vecesHoy > 7){
+        this.texto = "Ya cumpliste por hoy! Buen trabajo! "
+      }
     }
 
     save(h:any,m:any){
